15be5275d11a9972b938bc2994fbdadcba986d6c
Phillip Fuller <fulwave@gmail.com>
2022-02-12 10:09:10 -0500

    Updated and made various channel widths and delivery folder

--- BlackHole/BlackHole.h
+++ BlackHole/BlackHole.h
@@ -126,12 +126,12 @@ static pthread_mutex_t              gPlugIn_StateMutex                  = PTHREA
 static UInt32                       gPlugIn_RefCount                    = 0;
 static AudioServerPlugInHostRef     gPlugIn_Host                        = NULL;
 
-#define                             kBox_UID                            "BlackHole%ich_UID"
+#define                             kBox_UID                            "BlackHole7_1%ich_UID"
 static CFStringRef                  gBox_Name                           = NULL;
 static Boolean                      gBox_Acquired                       = true;
 
-#define                             kDevice_UID                         "BlackHole%ich_UID"
-#define                             kDevice_ModelUID                    "BlackHole%ich_ModelUID"
+#define                             kDevice_UID                         "BlackHole7_1%ich_UID"
+#define                             kDevice_ModelUID                    "BlackHole7_1%ich_ModelUID"
 static pthread_mutex_t              gDevice_IOMutex                     = PTHREAD_MUTEX_INITIALIZER;
 static Float64                      gDevice_SampleRate                  = 44100.0;
 static UInt64                       gDevice_IOIsRunning                 = 0;
@@ -153,7 +153,7 @@ static bool                         gMute_Master_Value                  = false;
 #define                             kManufacturer_Name                  "Existential Audio Inc."
 
 #define                             kLatency_Frame_Size                 0
-#define                             kNumber_Of_Channels                 2
+#define                             kNumber_Of_Channels                 8
 #define                             kBits_Per_Channel                   32
 #define                             kBytes_Per_Channel                  (kBits_Per_Channel/ 8)
 #define                             kBytes_Per_Frame                    (kNumber_Of_Channels * kBytes_Per_Channel)
